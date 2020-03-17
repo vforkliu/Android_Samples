ec7136646938cbb7b9f47a5cb9220acedf926775
liujun <liujun@forkliu.com>
2020-03-17 11:36:49 +0800

    finished tinder

---
+++ Tinder/app/src/main/java/com/forkliu/tinder/BindingUtils.kt
@@ -0,0 +1,9 @@
+package com.forkliu.tinder
+
+import androidx.databinding.BindingAdapter
+import com.facebook.drawee.view.SimpleDraweeView
+
+@BindingAdapter("image")
+fun loadImage(view: SimpleDraweeView, url: String) {
+    view.setImageURI(url)
+}
---
+++ Tinder/app/src/main/java/com/forkliu/tinder/Profile.kt
@@ -0,0 +1,9 @@
+package com.forkliu.tinder
+
+data class Profile(
+    val age: Int,
+    val distance: Int,
+    val id: Int,
+    val name: String,
+    val profile_pic: String
+)
